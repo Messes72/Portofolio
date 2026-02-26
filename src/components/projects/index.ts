9952338af71e9891e5310b256abbce6b8b4e22c4
Messes72 <85023670+Messes72@users.noreply.github.com>
2026-02-26 23:45:54 +0700

    [Phase 3.7] Fix duplicate export in projects index

--- src/components/projects/index.ts
+++ src/components/projects/index.ts
@@ -1,4 +1,3 @@
 export { ProjectCard } from "./ProjectCard";
 export { ProjectGrid } from "./ProjectGrid";
 export { GameCartridge } from "./GameCartridge";
-export { GameCartridge } from "./GameCartridge";
